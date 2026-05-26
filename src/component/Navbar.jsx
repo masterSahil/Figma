9b0e7a29fa9901860fc12f58dc0ea1c85bc1c5bf
masterSahil <7882mastersahil@gmail.com>
2026-05-26 16:14:10 +0530

    Navbar done

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
 import Logo from '/images/logo/logo.png'
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
   const links = [ "Home", "Pages", "Menu", "Order", "Blog", "Contact", "Shop" ];
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="fixed top-0 left-0 w-full z-50">
+    <nav className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${scrolled ? "bg-[#111111]/50 backdrop-blur-xl border-b shadow-lg" : "bg-transparent" }`}>
       <div className="max-w-360 mx-auto px-6">
-        <div className="flex items-center justify-between h-24">
+        <div className={`flex items-center justify-between transition-all duration-300 ${scrolled ? "h-20" : "h-24"}`}>
           <div>
-            <img src={Logo} alt="Nest Text Logo" className="max-h-14 object-contain" />
+            <img src={Logo} alt="Nest Text Logo" className="max-h-30 object-contain" />
           </div>
 
           <ul className="hidden lg:flex items-center gap-10 text-white">
